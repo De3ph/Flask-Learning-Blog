f8145688858216bc8108318552b3b636a96d5654
De3ph <05190000108@ogrenci.ege.edu.tr>
2021-09-04 02:52:03 +0300

    nothing important

--- blog/routes/route.py
+++ blog/routes/route.py
@@ -6,18 +6,21 @@ from blog.models.models import Member, Post
 from flask_login.mixins import UserMixin
 from flask_mail import Message
 
+
 @app.errorhandler(404)
 def error_404(error):
-    return render_template('errors/404.html') , 404
+    return render_template('errors/404.html'), 404
+
 
 @app.errorhandler(500)
 def error_500(error):
-    return render_template('errors/500.html') , 500
+    return render_template('errors/500.html'), 500
 
 
 @app.route('/')
 def home_page():
     return render_template('pages/home.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -26,17 +29,20 @@ def register_page():
 
     if form.validate_on_submit():
 
-        new_member = Member(form.username.data, form.email.data, form.password.data)
+        new_member = Member(form.username.data,
+                            form.email.data, form.password.data)
         db.session.add(new_member)
         db.session.commit()
 
-        flash(f'Registered successfuly. Welcome dear {new_member.username}.', category='register_ok')
+        flash(
+            f'Registered successfuly. Welcome dear {new_member.username}.', category='register_ok')
         return redirect(url_for('home_page'))
     if form.errors != {}:
         for message in form.errors.values():
             flash(message=f'{message[0]}', category='register_no')
 
-    return render_template('pages/register.html',form=form)
+    return render_template('pages/register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
@@ -45,21 +51,25 @@ def login_page():
 
     if form.validate_on_submit():
 
-        deneme_kullanıcı = Member.query.filter_by(username = form.username.data).first()
+        deneme_kullanıcı = Member.query.filter_by(
+            username=form.username.data).first()
 
         try:
             if deneme_kullanıcı.check_password(form.password.data):
                 login_user(deneme_kullanıcı)
-                flash(message=f'Logged in successfuly! Welcome {deneme_kullanıcı.username}',category='login_ok')
+                flash(
+                    message=f'Logged in successfuly! Welcome {deneme_kullanıcı.username}', category='login_ok')
                 session["username"] = request.form["username"]
-                return redirect(url_for('logged_page',username=session["username"]))
+                return redirect(url_for('logged_page', username=session["username"]))
+            else:
+                flash(message=f"Wrong password!", category='login_bad')
 
         except AttributeError:
-            flash(message=f"This member didn't exist!",category='login_bad')
+            flash(message=f"This member didn't exist!", category='login_bad')
             return redirect(url_for('login_page'))
 
-        
-    return render_template('pages/login.html',form=form)
+    return render_template('pages/login.html', form=form)
+
 
 @app.route('/logout')
 def logout():
@@ -67,9 +77,12 @@ def logout():
     flash(message=f'Logged out successfully!!', category='logout_ok')
     return redirect(url_for('home_page'))
 
+
 '''
     buradaki <username> base.html den geliyor, url_for fonksiyonunda parametre olarak
 '''
+
+
 @app.route('/<username>/post_create', methods=['GET', 'POST'])
 @login_required
 def post_create(username):
@@ -79,32 +92,34 @@ def post_create(username):
         new_post = Post(form.title.data, form.content.data, form.author.data)
         db.session.add(new_post)
         db.session.commit()
-        form.title.data="";
-        form.content.data="";
+        form.title.data = ""
+        form.content.data = ""
         flash(message='Post created successfully!', category='post_ok')
-        return redirect(url_for('logged_page' , username=username))
+        return redirect(url_for('logged_page', username=username))
     if form.errors != {}:
         for message in form.errors.values():
-            flash(message=f'Somethings gone wrong :( : {message} !', category='post_bad')
+            flash(
+                message=f'Somethings gone wrong :( : {message} !', category='post_bad')
 
     return render_template('pages/post_create.html', form=form)
 
-@app.route('/<username>/post/<post_title>', methods=['GET','POST'])
+
+@app.route('/<username>/post/<post_title>', methods=['GET', 'POST'])
 def delete_post_page(username, post_title):
-    
+
     try:
-        
+
         delete_post = Post.query.filter_by(title=post_title).first()
         db.session.delete(delete_post)
         db.session.commit()
-        flash('Writing deleted!',category='post_deleted')
-        return redirect(url_for('logged_page' , username=username))
+        flash('Writing deleted!', category='post_deleted')
+        return redirect(url_for('logged_page', username=username))
 
     except Exception as error:
         raise error
-    
 
-@app.route('/<username>/profile', methods=['GET','POST'])
+
+@app.route('/<username>/profile', methods=['GET', 'POST'])
 @login_required
 def logged_page(username):
 
@@ -117,9 +132,9 @@ def logged_page(username):
 
     if form.validate_on_submit():
         msg = Message('Subscription!', recipients=[form.email.data])
-        msg.body='Thanks for the mail subscription :)'
+        msg.body = 'Thanks for the mail subscription :)'
         mail.send(msg)
         flash(message=f'Mail sent successfully!!!', category='mail_ok')
         return redirect(url_for('logged_page'))
 
-    return render_template('pages/logged.html', form = form, posts = posts, post_num=post_num)
+    return render_template('pages/logged.html', form=form, posts=posts, post_num=post_num)

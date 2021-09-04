31fb8f3d321acce2414cf22482b5b69ffd945315
De3ph <05190000108@ogrenci.ege.edu.tr>
2021-09-05 01:47:45 +0300

    animation for flash messages

---
+++ blog/static/js/register.js
@@ -0,0 +1,6 @@
+const message_content = document.querySelector('.message').innerHTML;
+
+const message = document.querySelector('.message');
+
+message.addEventListener('mouseover' , ()=> message.innerHTML='X');
+message.addEventListener('mouseout' , ()=> message.innerHTML=message_content);

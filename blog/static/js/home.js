376ddbb4a5e0010001e4402244cceaef822c1ca8
De3ph <05190000108@ogrenci.ege.edu.tr>
2021-09-07 12:52:02 +0300

    loading screen added

--- blog/static/js/home.js
+++ blog/static/js/home.js
@@ -1,6 +1,13 @@
+let loading_screen = document.querySelector('.loader');
+
+window.addEventListener('load' , function(){
+    loading_screen.style.display = 'none';
+});
+
 const message_content = document.querySelector('.message').innerHTML;
 
 const message = document.querySelector('.message');
 
 message.addEventListener('mouseover' , ()=> message.innerHTML='X');
 message.addEventListener('mouseout' , ()=> message.innerHTML=message_content);
+

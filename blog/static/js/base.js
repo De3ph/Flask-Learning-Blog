f468a6ebd2f8dc038e632a1f33d29898ab60cf3e
De3ph <05190000108@ogrenci.ege.edu.tr>
2021-09-10 23:15:09 +0300

    dark theme made

--- blog/static/js/base.js
+++ blog/static/js/base.js
@@ -1,17 +1,55 @@
-let loading_screen = document.querySelector('.loader');
+const loading_screen = document.querySelector('.loader');
 
-window.addEventListener('load' , function(){
+const btn = document.querySelector('.darkMode');
+
+let cookieExist = document.cookie.split(';').some((item) => item.trim().startsWith('theme='))
+
+if (!cookieExist) {
+    document.cookie = 'theme=light';
+}
+
+
+window.addEventListener('load', function () {
+
+    if (document.cookie
+        .split('; ')
+        .find(row => row.startsWith('theme='))
+        .split('=')[1] == "light") {
+        document.body.classList.remove('none-background');
+        btn.innerHTML = 'Dark Theme';
+
+    }
+    else if (document.cookie
+        .split('; ')
+        .find(row => row.startsWith('theme='))
+        .split('=')[1] == "dark") {
+        document.body.classList.add('none-background');
+        btn.innerHTML = 'Light Theme';
+
+    }
     loading_screen.style.display = 'none';
 });
 
 
-let btn = document.querySelector('.darkMode')
+/* dark theme */
 
-let currentTheme = localStorage.getItem("theme")
-
-function turnBlack() {
-    // document.body.style.background = 'black';
-
-    document.body.style.background = 'none';
-    document.body.classList.add('bg-black');
-}
+btn.addEventListener("click", function () {
+    if (document.cookie
+        .split('; ')
+        .find(row => row.startsWith('theme='))
+        .split('=')[1] == "light") {
+        // enable dark mode 
+        document.body.classList.add('none-background');
+        document.cookie = 'theme=dark';
+        btn.innerHTML = 'Light Theme';
+    }
+    else if (document.cookie
+        .split('; ')
+        .find(row => row.startsWith('theme='))
+        .split('=')[1] == "dark") {
+        // enable light mode
+        document.body.classList.remove('none-background');
+        document.cookie = 'theme=light';
+        btn.innerHTML = 'Dark Theme';
+    }
+})
